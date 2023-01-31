0c31a8e4e3fa26642599bc2e6c0c97df22935ee5
keisukesugiura519 <s.keisuke519@gmail.com>
2023-02-01 01:28:37 +0900

    delete database

--- app/src/main/java/com/example/postsms2slack/Entity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.postsms2slack
-
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-
-
-@Entity(tableName = "slack_channelTable")
-data class slack_channel(@ColumnInfo(name = "channelID") val channelID: String, @ColumnInfo(name = "token") val token: String)
-
